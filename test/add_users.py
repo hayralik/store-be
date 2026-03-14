9eb1fd3b3f1e25ae05c19b04f05babd36e836e88
hayralik <hayralik@gmail.com>
2026-03-14 12:20:47 +0400

    DB added users

--- test/add_users.py
+++ test/add_users.py
@@ -3,7 +3,7 @@ import requests
 users = [
     {'id': 1, 'email': 'alik01@mail.com', 'password': 'Alik01'},
     {'id': 2, 'email': 'alik02@mail.com', 'password': 'Alik02'},
-    {'id': 3, 'email': 'alik03@mail.com', 'password': 'Alik3'}
+    {'id': 3, 'email': 'alik03@mail.com', 'password': 'Alik03'}
     ]
 url = "http://localhost:5000"
 
