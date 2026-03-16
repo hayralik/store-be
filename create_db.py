56770df39988e309fd8fc3e396de1cbf6dfc196e
hayralik <hayralik@gmail.com>
2026-03-17 00:06:52 +0400

    изменен create_db для Render

--- create_db.py
+++ create_db.py
@@ -39,5 +39,8 @@ def create_db(app):
         
         def __repr__(self):
             return f'<Product {self.name}>'
+        # Создаем таблицы, если их нет
+    with app.app_context():
+        db.create_all()
         
-    return db, User, Product
+    return db, User, Product    
